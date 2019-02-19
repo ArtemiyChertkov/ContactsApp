6e1025489fb6e939370509aff2d8e346cd63bb9b
pro_baty00 <pro_baty00@mail.ru>
2019-02-19 13:30:23 +0700

    2 лаба , переделал

---
+++ ContactsAppUI/ContactsApp/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// класс проекта
+    /// </summary>
+    public class Project
+    {
+        /// <summary>
+        /// Список контактов
+        /// </summary>
+        public List<Contact> _contactslistone = new List<Contact>();
+    }
+}
